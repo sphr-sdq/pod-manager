e6c891d8bed713cc4a830f46da5d97827c3867b3
Sepehr Sadeghpour <sepehr.sad78@gmail.com>
2024-10-14 00:18:57 +0330

    setup unplug-icon
    //TODO: set default english font

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/login2' , function (){
+    return Inertia::render('Login');
+});
+
 require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
     plugins: [
+        Icons({ autoInstall: true,}),
         laravel({
             input: 'resources/js/app.js',
             ssr: 'resources/js/ssr.js',
