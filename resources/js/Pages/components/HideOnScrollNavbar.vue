1f8b0ed0e451b1526ec1647f0265feb18ab749be
Sepehr Sadeghpour <sepehr.sad78@gmail.com>
2024-10-16 13:03:18 +0330

    HideOnScrollNavbar component added

---
+++ resources/js/Pages/components/HideOnScrollNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <header v-vue-scrollup-menu class="header"></header>
+</template>
+
+<script >
+import vueScrollupMenu from 'vue-scrollup-menu'
+import {withDirectives} from "vue";
+
+export default {
+    name: 'Header',
+
+    directives: {
+        vueScrollupMenu
+    },
+}
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+    background-color: rgba(0, 0, 0, .4);
+    height: 90px;
+    width: 100%;
+
+    overflow: auto;
+    transition: top 0.2s ease-in-out;
+    position: fixed;
+    z-index: 5;
+}
+</style>
