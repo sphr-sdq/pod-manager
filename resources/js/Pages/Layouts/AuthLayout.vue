393f94b05c66dc342ebf2d10f8baaa334e53d5f9
Sepehr Sadeghpour <sepehr.sad78@gmail.com>
2024-10-17 14:46:31 +0330

    login page finished. ready for signup page

--- resources/js/Pages/Layouts/AuthLayout.vue
+++ resources/js/Pages/Layouts/AuthLayout.vue
@@ -1,23 +1,38 @@
 <script setup lang="js">
-
+import {Button} from '@/components/ui/button'
+import House from "~icons/lucide/House"
+import { router } from '@inertiajs/vue3'
 </script>
 
 <template>
-<main vaul-drawer-wrapper class="bg-white">
-    <div class="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0
+    <main vaul-drawer-wrapper class="bg-white">
+        <div class="container relative hidden h-screen flex-col  justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0
 ">
-        <div class="lg:p-8">
-            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                <slot />
+
+            <div class="flex flex-col ">
+                <div class="container mt-6">
+                    <div>
+                        <Button @click.prevent="router.visit('/')" variant="outline" size="icon">
+
+                                <House class="w-4 h-4"/>
+
+                        </Button>
+
+                    </div>
+                </div>
+                <div class="lg:p-8 flex-grow content-center">
+                    <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                        <slot/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+                <div class="bg-cover absolute inset-0 bg-zinc-900
+            bg-[url('https://images.unsplash.com/photo-1687300172792-68a13c4e149a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
+"/>
+
             </div>
         </div>
-
-        <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div class="bg-cover absolute inset-0 bg-zinc-900
-            bg-[url('https://images.unsplash.com/photo-1687300172792-68a13c4e149a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
-" />
-
-        </div>
-    </div>
-</main>
+    </main>
 </template>
--- resources/js/Pages/components/UserAuthForm.vue
+++ resources/js/Pages/components/UserAuthForm.vue
@@ -1,18 +1,16 @@
 <script setup lang="js">
-import { ref } from 'vue'
+import {ref} from 'vue'
 import LucideSpinner from '~icons/lucide/loader-2'
-import GitHubLogo from '~icons/radix-icons/github-logo'
 
-import { cn } from '@/lib/utils'
-import { Button } from '@/Components/ui/button'
-import { Input } from '@/Components/ui/input'
-import { Label } from '@/Components/ui/label'
-import { Link, useForm } from '@inertiajs/vue3'
-
-import DemoDrawer from '../components/DemoDrawer.vue'
+import {cn} from '@/lib/utils'
+import {Button} from '@/Components/ui/button'
+import {Input} from '@/Components/ui/input'
+import {Label} from '@/Components/ui/label'
+import {Link, useForm} from '@inertiajs/vue3'
 
 
 const isLoading = ref(false)
+
 async function onSubmit(event) {
     event.preventDefault()
     isLoading.value = true
@@ -24,12 +22,26 @@ async function onSubmit(event) {
 </script>
 
 <template>
-    <div :class="cn('grid gap-6', $attrs.class ?? '')" >
+    <div :class="cn('grid gap-6', $attrs.class ?? '')">
+
+        <div class="">
+            <div class="flex flex-row items-baseline gap-2 mb-2">
+                    <div class="w-3 h-3 border-2 border-black p-0.5">
+                        <div class="w-full h-full bg-black"></div>
+                    </div>
+                <h1 class="text-3xl font-bold">ورود</h1>
+            </div>
+
+            <h3 class="text-sm text-muted-foreground">
+                برای ورود به حساب کاربری شماره موبایل و رمز عبور خود را وارد کنید.
+            </h3>
+        </div>
+
         <form @submit="onSubmit">
             <div class="grid gap-2">
                 <div class="grid gap-1">
                     <Label class="" for="email">
-                        شماه همراه
+                        شماه موبایل
                     </Label>
                     <Input
                         id="email"
@@ -57,14 +69,15 @@ async function onSubmit(event) {
                     />
                 </div>
                 <Button :disabled="isLoading">
-                    <LucideSpinner v-if="isLoading" class="mr-2 h-4 w-4 animate-spin" />
+                    <LucideSpinner v-if="isLoading" class="mr-2 h-4 w-4 animate-spin"/>
                     ورود
                 </Button>
             </div>
         </form>
+
         <div class="relative">
             <div class="absolute inset-0 flex items-center">
-                <span class="w-full border-t" />
+                <span class="w-full border-t"/>
             </div>
             <div class="relative flex justify-center text-xs uppercase">
         <span class="bg-background px-2 text-muted-foreground">
@@ -72,7 +85,8 @@ async function onSubmit(event) {
         </span>
             </div>
         </div>
-        <p class="px-8 text-center text-sm text-muted-foreground">
+
+        <p class=" text-sm text-muted-foreground">
             در صورتی که رمز عبور خود را فراموش کرده‌اید برای ورود با رمز یکبار مصرف
             <a
                 href="/terms"
@@ -82,16 +96,15 @@ async function onSubmit(event) {
             </a>
 
         </p>
-        <p class="px-8 text-center text-sm text-muted-foreground">
-             هنوز اکانت ندارید ؟ برای ساخت اکانت
+        <p class="text-sm text-muted-foreground">
+            هنوز اکانت ندارید ؟ برای ساخت اکانت
             <Link
                 href="/register"
                 class="underline underline-offset-4 hover:text-primary"
             >
-               کلیک
+                کلیک
             </Link>
             کنید.
         </p>
-        <DemoDrawer />
     </div>
 </template>
